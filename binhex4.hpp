9b6a60f7fc0fca912cbce3b5a5ac10b160d72ca4
ysy-255 <ysy-255@outlook.com>
2025-05-11 15:13:17 +0900

    保全活動

--- binhex4.hpp
+++ binhex4.hpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <array>
 
 #include "FILE.hpp"
 
 namespace binhex4{
-	std::vector<uint8_t> table = {
-		0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-		0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+	/* !"#$%&'()*+,-012345689@ABCDEFGHIJKLMNPQRSTUVXYZ[`abcdefhijklmpqr */
+	const std::array<uint8_t, 0x53> table = {
 		   0,0x00,0x01,0x02, 0x03,0x04,0x05,0x06, 0x07,0x08,0x09,0x0A, 0x0B,0x0C,   0,   0,
 		0x0D,0x0E,0x0F,0x10, 0x11,0x12,0x13,   0, 0x14,0x15,   0,   0,    0,   0,   0,   0,
 		0x16,0x17,0x18,0x19, 0x1A,0x1B,0x1C,0x1D, 0x1E,0x1F,0x20,0x21, 0x22,0x23,0x24,   0,
@@ -13,35 +13,28 @@ namespace binhex4{
 		0x30,0x31,0x32,0x33, 0x34,0x35,0x36,   0, 0x37,0x38,0x39,0x3A, 0x3B,0x3C,   0,   0,
 		0x3D,0x3E,0x3F
 	};
-	std::string correct_comment = "(This file must be converted with BinHex 4.0)";
+	const std::string correct_comment = "(This file must be converted with BinHex 4.0)";
 	std::vector<uint8_t> read_simple(const std::string & path){
-		std::ifstream ifs = std::ifstream(path);
-		std::string comment;
-		char ch;
-		while(ifs.get(ch)){
-			if(ch == '\n' || ch == '\r') break;
-			else comment.push_back(ch);
-		}
-		comment.resize(correct_comment.size());
-		if(comment != correct_comment) return {};
-		char start;
-		ifs >> start;
-		if(start != ':') return {};
-		std::string raw, line;
-		while(ifs >> line){
-			raw += line;
-		}
-		if(raw.size() < 2) return {};
-		if(raw.back() == ':') raw.pop_back();
-		std::vector<uint8_t> stream; stream.reserve(raw.size());
+		const auto raw = readFile(path);
+		auto l = raw.begin(), r = raw.end();
+		l = std::find(l, r, '(');
+		if(r - l < correct_comment.size() + 3) return {};
+		if(!std::equal(correct_comment.begin(), correct_comment.end(), l)) return {};
+		l += correct_comment.size();
+		l = std::find(l, r, ':');
+		if(r - l < 2) return {};
+		l ++;
+		while(l != r) if(*--r == ':') break;
+		std::vector<uint8_t> stream; stream.reserve(r - l);
+
 		bool mode90 = false;
 		auto f = [&](){
 			if(mode90){
 				uint8_t times = stream.back();
-				stream.pop_back();
+				stream.pop_back(); // times
 				if(times != 0){
 					times --;
-					stream.pop_back();
+					stream.pop_back(); // 0x90
 					uint8_t ch = stream.back();
 					stream.resize(stream.size() + times, ch);
 				}
@@ -51,25 +44,41 @@ namespace binhex4{
 				mode90 = true;
 			}
 		};
-		for(auto itr = raw.begin(), end = raw.begin() + (raw.size() >> 2 << 2); itr != end; itr += 4){
-			stream.emplace_back(table.at(*(itr + 0)) << 2 | table.at(*(itr + 1)) >> 4);
-			f();
-			stream.emplace_back(table.at(*(itr + 1)) << 4 | table.at(*(itr + 2)) >> 2);
-			f();
-			stream.emplace_back(table.at(*(itr + 2)) << 6 | table.at(*(itr + 3)) >> 0);
-			f();
+		for(uint8_t cnt = 0; l != r; l++){
+			uint8_t ch = *l;
+			ch -= 0x20;
+			if(ch >= 0x53) continue;
+			ch = table[ch];
+			switch(cnt++ & 0b11){
+				case 0:{
+					stream.push_back(ch << 2);
+					break;
+				}
+				case 1:{
+					stream.back() |= ch >> 4;
+					f();
+					stream.push_back(ch << 4);
+					break;
+				}
+				case 2:{
+					stream.back() |= ch >> 2;
+					f();
+					stream.push_back(ch << 6);
+					break;
+				}
+				case 3:{
+					stream.back() |= ch >> 0;
+					f();
+					break;
+				}
+			}
 		}
-		// ↑あまりは無視しました
-		
+
 		size_t idx = stream.front() + 12;
 		const uint8_t* i = &stream[idx];
 		size_t sz = readData<uint32_t>(i, false);
-		idx += 10;
-		std::vector<uint8_t> res;
-		res.reserve(sz);
-		for(size_t i = 0; i < sz; ++i){
-			res.emplace_back(stream[idx + i]);
-		}
-		return res;
+		size_t res_l = std::min(idx + 10, stream.size());
+		size_t res_r = std::min(idx + 10 + sz, stream.size());
+		return std::vector<uint8_t>(stream.begin() + res_l, stream.begin() + res_r);
 	}
 }
