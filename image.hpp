a69d4fb4f7fae993085a6b65c41d5af243bba166
ysy-255 <ysy-255@outlook.com>
2025-06-20 22:44:33 +0900

    8ビットのみを対象とするようにした

--- image.hpp
+++ image.hpp
@@ -1,114 +1,118 @@
 #ifndef IMAGE_HPP
 #define IMAGE_HPP
 
-#include <cstdint>
 #include <vector>
 
-template<typename T>
-class RGB;
+#include "int.hpp"
 
-template<typename T>
-class RGBA;
+struct RGB8;
+struct RGBA8;
+class Image_RGB8;
+class Image_RGBA8;
 
-
-template<typename T>
-class IMAGE_RGB;
-
-template<typename T>
-class IMAGE_RGBA;
-
-
-template<typename T>
-class RGB{
-	public:
-	T R = 0;
-	T G = 0;
-	T B = 0;
-	RGB& operator=(const RGBA<T> & other){
-		R = other.R;
-		G = other.G;
-		B = other.B;
-		return *this;
-	}
+struct RGB8{
+	u8 R = 0;
+	u8 G = 0;
+	u8 B = 0;
+	RGB8& operator=(const RGBA8 & other);
 };
 
-template<typename T>
-class RGBA{
-	public:
-	T R = 0;
-	T G = 0;
-	T B = 0;
-	T A = 0xFF;
-	RGBA& operator=(const RGB<T> & other){
-		R = other.R;
-		G = other.G;
-		B = other.B;
-		A = 0xFF;
-		return *this;
-	}
+struct RGBA8{
+	u8 R = 0;
+	u8 G = 0;
+	u8 B = 0;
+	u8 A = U8MAX;
+	RGBA8& operator=(const RGB8 & other);
 };
 
+RGB8& RGB8::operator=(const RGBA8 & other){
+	R = other.R;
+	G = other.G;
+	B = other.B;
+	return *this;
+}
 
-template<typename T>
-class IMAGE_RGB{
-	public:
-	friend class IMAGE_RGBA<T>;
+RGBA8& RGBA8::operator=(const RGB8 & other){
+	R = other.R;
+	G = other.G;
+	B = other.B;
+	A = U8MAX;
+	return *this;
+}
 
-	IMAGE_RGB(){}
-	IMAGE_RGB(const uint32_t W, const uint32_t H) :
-		width(W), height(H),
-		data(std::vector<std::vector<RGB<T>>>(H, std::vector<RGB<T>>(W))) {}
-	IMAGE_RGB(const IMAGE_RGBA<T> & img) : width(img.width), height(img.height){
-		data = std::vector<std::vector<RGB<T>>>(height, std::vector<RGB<T>>(width));
-		for(uint32_t h = 0; h < height; ++h){
-			for(uint32_t w = 0; w < width; ++w){
-				data[h][w] = img.data[h][w];
-			}
+class Image_RGB8{
+public:
+	friend class Image_RGBA8;
+
+	size_t H, W;
+
+	Image_RGB8() = default;
+	Image_RGB8(size_t Height, size_t Width) : H(Height), W(Width), data(H, std::vector<RGB8>(W)) {}
+	Image_RGB8(const Image_RGBA8 & img);
+
+	Image_RGB8 & operator=(const Image_RGBA8 & other);
+
+	std::vector<RGB8> & operator[](const size_t h){ return data[h]; }
+
+
+protected:
+	std::vector<std::vector<RGB8>> data;
+};
+
+class Image_RGBA8{
+public:
+	friend class Image_RGB8;
+
+	size_t H, W;
+
+	Image_RGBA8() = default;
+	Image_RGBA8(size_t Height, size_t Width) : H(Height), W(Width), data(H, std::vector<RGBA8>(W)) {}
+	Image_RGBA8(const Image_RGB8 & img);
+
+	Image_RGBA8 & operator=(const Image_RGB8 & other);
+
+	std::vector<RGBA8> & operator[](size_t h){ return data[h]; }
+
+
+protected:
+	std::vector<std::vector<RGBA8>> data;
+};
+
+Image_RGB8::Image_RGB8(const Image_RGBA8 & img) : H(img.H), W(img.W), data(H, std::vector<RGB8>(W)){
+	for(size_t h = 0; h < H; ++h){
+		for(size_t w = 0; w < W; ++w){
+			data[h][w] = img.data[h][w];
 		}
 	}
-
-	inline std::vector<RGB<T>> & operator[](size_t h){
-		return data[h];
-	}
-
-	uint32_t width;
-	uint32_t height;
-
-	protected:
-
-	std::vector<std::vector<RGB<T>>> data;
-};
-
-
-
-template<typename T>
-class IMAGE_RGBA{
-	public:
-	friend class IMAGE_RGB<T>;
-
-	IMAGE_RGBA(){}
-	IMAGE_RGBA(const uint32_t W, const uint32_t H) :
-		width(W), height(H),
-		data(std::vector<std::vector<RGBA<T>>>(H, std::vector<RGBA<T>>(W))) {}
-	IMAGE_RGBA(const IMAGE_RGB<T> & img) : width(img.width), height(img.height){
-		data = std::vector<std::vector<RGBA<T>>>(height, std::vector<RGBA<T>>(width));
-		for(uint32_t h = 0; h < height; ++h){
-			for(uint32_t w = 0; w < width; ++w){
-				data[h][w] = img.data[h][w];
-			}
+}
+Image_RGB8 & Image_RGB8::operator=(const Image_RGBA8 & other){
+	H = other.H;
+	W = other.W;
+	data = std::vector<std::vector<RGB8>>(H, std::vector<RGB8>(W));
+	for(size_t h = 0; h < H; ++h){
+		for(size_t w = 0; w < W; ++w){
+			data[h][w] = other.data[h][w];
 		}
 	}
-
-	inline std::vector<RGBA<T>> & operator[](size_t h){
-		return data[h];
+	return *this;
+}
+Image_RGBA8::Image_RGBA8(const Image_RGB8 & img) : H(img.H), W(img.W), data(H, std::vector<RGBA8>(W)){
+	for(size_t h = 0; h < H; ++h){
+		for(size_t w = 0; w < W; ++w){
+			data[h][w] = img.data[h][w];
+		}
 	}
-
-	uint32_t width;
-	uint32_t height;
-
-	protected:
-
-	std::vector<std::vector<RGBA<T>>> data;
-};
+}
+Image_RGBA8 & Image_RGBA8::operator=(const Image_RGB8 & other){
+	H = other.H;
+	W = other.W;
+	data = std::vector<std::vector<RGBA8>>(H, std::vector<RGBA8>(W));
+	for(size_t h = 0; h < H; ++h){
+		for(size_t w = 0; w < W; ++w){
+			data[h][w] = other.data[h][w];
+		}
+	}
+	return *this;
+}
 
 #endif
